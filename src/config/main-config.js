f1c0878fda82901a322fe8bef80b1f5548859a3c
Nick <nbjames2@gmail.com>
2018-12-17 20:22:13 -0500

    fix

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -10,6 +10,6 @@ module.exports = {
         app.set("view engine", "ejs");
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(express.static(path.join(__dirname, "..", "assets")));
-        app.use(expressValidator);
+        app.use(expressValidator());
     }
 };
