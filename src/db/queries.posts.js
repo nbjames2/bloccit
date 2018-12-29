a2100618a5c8bc1a0acb9d799da9a5f068451188
Nick <nbjames2@gmail.com>
2018-12-28 21:06:18 -0500

    comments checkpoint complete

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -1,5 +1,7 @@
 const Post = require("./models").Post;
 const Authorizer = require("../policies/post");
+const Comment = require("./models").Comment;
+const User = require("./models").User;
 
 
 module.exports = {
@@ -13,7 +15,13 @@ module.exports = {
         })
     },
     getPost(id, callback){
-        return Post.findById(id)
+        return Post.findById(id, {
+            include: [
+                {model: Comment, as: "comments", include: [
+                    {model: User }
+                ]}
+            ]
+        })
         .then((post) => {
           callback(null, post);
         })
